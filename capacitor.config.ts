458ca26af3747155862959591b1a05598a0ad9da
GABRIEL SULDINE PEREIRA <03569198200@ifms.edu.br>
2023-08-22 19:45:30 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'systempad-front',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
